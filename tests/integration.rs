6d8d7b783a8c4be62792e39c34c4a7c70d1664b2
Diego Barrios Romero <eldruin@gmail.com>
2020-03-10 18:56:56 +0100

    Extract common macro

--- tests/integration.rs
+++ tests/integration.rs
@@ -9,53 +9,25 @@ fn can_create_and_destroy() {
     destroy(sensor);
 }
 
-#[test]
-fn can_read_ambient_temperature() {
-    let mut sensor = new_mlx90614(&[I2cTrans::write_read(
-        ADDR,
-        vec![mlx90614::Register::TA],
-        vec![225, 57, 233],
-    )]);
-    let t = sensor.ambient_temperature().unwrap();
-    assert_near!(t, 23.19, 0.1);
-    destroy(sensor);
+macro_rules! read_ambient_temp_test {
+    ($name:ident, $data0:expr, $data1:expr, $data2:expr, $expected:expr) => {
+        #[test]
+        fn $name() {
+            let mut sensor = new_mlx90614(&[I2cTrans::write_read(
+                ADDR,
+                vec![mlx90614::Register::TA],
+                vec![$data0, $data1, $data2],
+            )]);
+            let t = sensor.ambient_temperature().unwrap();
+            assert_near!(t, $expected, 0.1);
+            destroy(sensor);
+        }
+    };
 }
-
-#[test]
-fn can_read_ambient_temperature2() {
-    let mut sensor = new_mlx90614(&[I2cTrans::write_read(
-        ADDR,
-        vec![mlx90614::Register::TA],
-        vec![97, 58, 86],
-    )]);
-    let t = sensor.ambient_temperature().unwrap();
-    assert_near!(t, 25.75, 0.1);
-    destroy(sensor);
-}
-
-#[test]
-fn can_read_ambient_temperature3() {
-    let mut sensor = new_mlx90614(&[I2cTrans::write_read(
-        ADDR,
-        vec![mlx90614::Register::TA],
-        vec![107, 58, 212],
-    )]);
-    let t = sensor.ambient_temperature().unwrap();
-    assert_near!(t, 25.95, 0.1);
-    destroy(sensor);
-}
-
-#[test]
-fn can_read_ambient_temperature4() {
-    let mut sensor = new_mlx90614(&[I2cTrans::write_read(
-        ADDR,
-        vec![mlx90614::Register::TA],
-        vec![38, 58, 102],
-    )]);
-    let t = sensor.ambient_temperature().unwrap();
-    assert_near!(t, 24.57, 0.1);
-    destroy(sensor);
-}
+read_ambient_temp_test!(can_read_ambient_temp1, 225, 57, 233, 23.19);
+read_ambient_temp_test!(can_read_ambient_temp2, 97, 58, 86, 25.75);
+read_ambient_temp_test!(can_read_ambient_temp3, 107, 58, 212, 25.95);
+read_ambient_temp_test!(can_read_ambient_temp4, 38, 58, 102, 24.57);
 
 #[test]
 fn read_ambient_temperature_crc_mismatch() {
