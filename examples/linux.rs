cb0365cdaf3a40402e0ce0eef788d6b42bf8990d
Diego Barrios Romero <eldruin@gmail.com>
2020-07-28 18:38:22 +0200

    Make code horizontally slightly shorter

--- examples/linux.rs
+++ examples/linux.rs
@@ -3,7 +3,8 @@ use mlx9061x::{Mlx9061x, SlaveAddr};
 
 fn main() {
     let dev = I2cdev::new("/dev/i2c-1").unwrap();
-    let mut sensor = Mlx9061x::new_mlx90614(dev, SlaveAddr::default(), 5).unwrap();
+    let addr = SlaveAddr::default();
+    let mut sensor = Mlx9061x::new_mlx90614(dev, addr, 5).unwrap();
     loop {
         let obj_temp = sensor.object1_temperature().unwrap();
         println!("Object temperature: {:.2}ºC", obj_temp);
