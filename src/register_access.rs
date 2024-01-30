19679b2e64fe26c2d2c10e2533f145ccd9ed04d0
hacknus <linus.stoeckli@unibe.ch>
2024-01-30 13:00:18 +0100

    implement read_i16 for raw readings as specified in datasheet

--- src/register_access.rs
+++ src/register_access.rs
@@ -7,7 +7,9 @@ pub mod mlx90614 {
     pub const SLEEP_COMMAND: u8 = 0xFF;
     pub const WAKE_DELAY_MS: u8 = 33;
     pub const DEV_ADDR: u8 = 0x5A;
+
     pub struct Register {}
+
     impl Register {
         pub const RAW_IR1: u8 = 0x04;
         pub const RAW_IR2: u8 = 0x05;
@@ -26,7 +28,9 @@ pub mod mlx90615 {
     pub const SLEEP_COMMAND: u8 = 0xC6;
     pub const WAKE_DELAY_MS: u8 = 39;
     pub const DEV_ADDR: u8 = 0x5B;
+
     pub struct Register {}
+
     impl Register {
         pub const RAW_IR: u8 = 0x05 | RAM_COMMAND;
         pub const TA: u8 = 0x06 | RAM_COMMAND;
@@ -58,6 +62,40 @@ where
             pec,
         )?;
         Ok(u16::from(data[0]) | (u16::from(data[1]) << 8))
+    }
+
+    fn msb_lsb_to_sign_magnitude(&self, msb: u8, lsb: u8) -> i16 {
+        // Extract the sign bit from the MSB
+        let sign_bit = msb & 0b10000000;
+
+        // Combine the remaining bits from MSB and LSB
+        let value = ((msb & 0b01111111) as i16) << 8 | lsb as i16;
+
+        // If the sign bit is set, make the value negative
+        if sign_bit != 0 {
+            -value
+        } else {
+            value
+        }
+    }
+
+    pub(crate) fn read_i16(&mut self, register: u8) -> Result<i16, Error<E>> {
+        let mut data = [0; 3];
+        self.i2c
+            .write_read(self.address, &[register], &mut data)
+            .map_err(Error::I2C)?;
+        let pec = data[2];
+        Self::check_pec(
+            &[
+                self.address << 1,
+                register,
+                (self.address << 1) + 1,
+                data[0],
+                data[1],
+            ],
+            pec,
+        )?;
+        Ok(self.msb_lsb_to_sign_magnitude(data[0], data[1]))
     }
 
     pub(crate) fn write_u8(&mut self, command: u8) -> Result<(), Error<E>> {
