ce9ee57fedba609c8e028682d12c531cb9318537
hacknus <33124824+hacknus@users.noreply.github.com>
2025-03-14 10:36:35 +0100

    Update src/register_access.rs
    Co-authored-by: Diego Barrios Romero <eldruin@gmail.com>

--- src/register_access.rs
+++ src/register_access.rs
@@ -91,7 +91,7 @@ where
         let sign_bit = msb & 0b1000_0000;
 
         // Combine the remaining bits from MSB and LSB
-        let value = ((msb & 0b01111111) as i16) << 8 | lsb as i16;
+        let value = ((msb & 0b0111_1111) as i16) << 8 | lsb as i16;
 
         if sign_bit != 0 {
             -value
