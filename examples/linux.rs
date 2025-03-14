3464cf622b1ca44c3858acb0773ad24872e90ec9
hacknus <linus.stoeckli@unibe.ch>
2025-03-14 11:11:05 +0100

    only run linux example on linux platforms (prevents build error on other platforms)

--- examples/linux.rs
+++ examples/linux.rs
@@ -1,6 +1,9 @@
+#[cfg(target_os = "linux")]
 use linux_embedded_hal::I2cdev;
+#[cfg(target_os = "linux")]
 use mlx9061x::{Mlx9061x, SlaveAddr};
 
+#[cfg(target_os = "linux")]
 fn main() {
     let dev = I2cdev::new("/dev/i2c-1").unwrap();
     let addr = SlaveAddr::default();
@@ -9,4 +12,9 @@ fn main() {
         let obj_temp = sensor.object1_temperature().unwrap();
         println!("Object temperature: {:.2}ºC", obj_temp);
     }
+}
+
+#[cfg(not(target_os = "linux"))]
+fn main() {
+    panic!("This example can only be run on Linux.");
 }
